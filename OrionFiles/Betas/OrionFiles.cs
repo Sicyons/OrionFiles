70030cb763c2f2a7fbe0d7b14cd5a08e7c9aee71
Franck <franck.deniau@auxitec.fr>
2018-03-29 15:57:18 +0200

    Project creation & initialization

---
+++ OrionFiles/Betas/OrionFiles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OrionCore.ErrorManagement;
+
+namespace OrionFiles
+{
+    public abstract class OrionFiles : IOrionErrorLogManager
+    {
+        #region Parent method implementations
+        public abstract Boolean LogError(StructOrionErrorLogInfos errorLog);
+        #endregion
+    }
+}
